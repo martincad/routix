b61f469c6bd0b0f5301b89627199af50a699782a
martincad <martincad>
2004-03-13 21:31:46 +0000

    *** empty log message ***

--- tareas/fork1/tarea.c
+++ tareas/fork1/tarea.c
@@ -11,13 +11,13 @@ void main(void)
 
 	for (j=1 ; j<10 ; j++) {
 	
-		for( i=0 ; i<0xfffff ; i++);
+		for( i=0 ; i<0x1fffff ; i++);
 		exec("rotor1.bin");
-		for( i=0 ; i<0xfffff ; i++);
+		for( i=0 ; i<0x1fffff ; i++);
 		exec("rotor2.bin");
-		for( i=0 ; i<0xfffff ; i++);
+		for( i=0 ; i<0x1fffff ; i++);
 		exec("rotor3.bin");
-		for( i=0 ; i<0xfffff ; i++);
+		for( i=0 ; i<0x1fffff ; i++);
 		exec("rotor4.bin");
 
 		printf("\nStress info: se han logrado ejecutar %d procesos\n", 4 * j);

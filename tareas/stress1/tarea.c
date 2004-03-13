d47f889a98fe2a8cccd07e5ea9cb6d5bff72f428
martincad <martincad>
2004-03-13 21:41:56 +0000

    *** empty log message ***

---
+++ tareas/stress1/tarea.c
@@ -0,0 +1,28 @@
+/* tarea.c */
+#include "stdarg.h"
+#include "../include/routix.h"
+
+char msg[40];
+
+void main(void) 
+{
+	int i=0;
+	int j=1;
+
+	for (j=1 ; j<10 ; j++) {
+	
+		for( i=0 ; i<0x1fffff ; i++);
+		exec("rotor1.bin");
+		for( i=0 ; i<0x1fffff ; i++);
+		exec("rotor2.bin");
+		for( i=0 ; i<0x1fffff ; i++);
+		exec("rotor3.bin");
+		for( i=0 ; i<0x1fffff ; i++);
+		exec("rotor4.bin");
+
+		printf("\nStress info: se ha logrado ejecutar %d procesos\n", 4 * j);
+	}
+    
+    while(1);
+}
+
